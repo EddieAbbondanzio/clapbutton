174e0010063e47d351771eee8203f3204e3eefb9
Ed Abbondanzio <me@eddieabbondanz.io>
2019-12-11 20:11:31 -0500

    Feature: CSS is now compiled into a seperate file upon build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const isDev = process.env.NODE_ENV == 'development';
 
 module.exports = {
   entry: "./src/js/index.js",
@@ -14,13 +15,24 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
-    }),
+    })
   ],
   module: {
     rules: [
       {
         test: /\.(s*)css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        
+        use: !isDev ? [
+          "style-loader",
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].css"
+            }
+          },
+          "sass-loader"
+        ] 
+        : ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
